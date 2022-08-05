f8209d2ecdad0ca41e370e36d3e69757836943e1
유재형 <wogud758787@naver.com>
2022-08-05 22:41:55 +0900

    feat : option 의 이름 자동 생성 기능 추가

--- src/main/kotlin/nhncommerce/project/option/domain/UpdateOptionDTO.kt
+++ src/main/kotlin/nhncommerce/project/option/domain/UpdateOptionDTO.kt
@@ -5,8 +5,5 @@ import nhncommerce.project.product.domain.Product
 data class UpdateOptionDTO (
     var product : Product?=null,
     var optionTypeList : Array<Option?> = Array<Option?>(3, {null}),
-//    var optionType1List : MutableList<Option> = ArrayList<Option>(),
-//    var optionType2List : MutableList<Option> = ArrayList<Option>(),
-//    var optionType3List : MutableList<Option> = ArrayList<Option>(),
     var optionNameList : List<MutableList<Option>?> = ArrayList<ArrayList<Option>?>()
 )
