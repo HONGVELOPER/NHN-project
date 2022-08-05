e126474046ce3c0403d99048b368cbf78df859fe
Youngjin Hong <hyjinn1003@naver.com>
2022-08-06 00:02:55 +0900

    Feat: 유저 dto validation 처리 및 컨트롤러 ModelAndView 객체로 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
 
-    id("org.springframework.boot") version "2.7.2"
-    id("io.spring.dependency-management") version "1.0.12.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
+	id("org.springframework.boot") version "2.7.2"
+	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
 
-    //---------
-    id ("org.jetbrains.kotlin.plugin.allopen") version "1.6.21" //allOpen, open 키워드 적용
-    id ("org.jetbrains.kotlin.plugin.noarg") version "1.6.21" // Entity에 no-arg 생성자 생성
-    //----------
+	//---------
+	id ("org.jetbrains.kotlin.plugin.allopen") version "1.6.21" //allOpen, open 키워드 적용
+	id ("org.jetbrains.kotlin.plugin.noarg") version "1.6.21" // Entity에 no-arg 생성자 생성
+	//----------
 
-    //Query dsl
-    kotlin("kapt") version "1.7.10"
+	//Query dsl
+	kotlin("kapt") version "1.7.10"
 
 }
 
@@ -24,73 +24,73 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    runtimeOnly("mysql:mysql-connector-java")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation ("org.springframework.boot:spring-boot-starter-oauth2-client:2.6.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	runtimeOnly("mysql:mysql-connector-java")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation ("org.springframework.boot:spring-boot-starter-oauth2-client:2.6.2")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "junit", module = "junit")
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "junit", module = "junit")
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    // MockK
-    testImplementation("io.mockk:mockk:1.12.1")
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	// MockK
+	testImplementation("io.mockk:mockk:1.12.1")
+	testImplementation("org.junit.jupiter:junit-jupiter")
 
-    //Query dsl
-    val querydslVersion = "5.0.0" //querydsl
-    implementation("com.querydsl:querydsl-jpa:$querydslVersion")
-    kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
+	//Query dsl
+	val querydslVersion = "5.0.0" //querydsl
+	implementation("com.querydsl:querydsl-jpa:$querydslVersion")
+	kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
 
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 //Query dsl
 sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
-    kotlin.srcDir("$buildDir/generated/source/kapt/main")
+	kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
--- src/main/kotlin/nhncommerce/project/user/domain/PasswordDTO.kt
+++ src/main/kotlin/nhncommerce/project/user/domain/PasswordDTO.kt
@@ -1,10 +1,16 @@
 package nhncommerce.project.user.domain
 
+import javax.validation.constraints.NotBlank
+
 
 data class PasswordDTO(
-    var password: String = "",
-    var newPassword: String = "",
-    var newPasswordVerify: String = "",
-) {
 
-}
+    @field:NotBlank(message = "비밀번호를 입력해주세요.")
+    var password: String = "",
+
+    @field:NotBlank(message = "새로운 비밀번호를 입력해주세요.")
+    var newPassword: String = "",
+
+    @field:NotBlank(message = "새로운 비밀번호를 한번 더 입력해주세요.")
+    var newPasswordVerify: String = "",
+) {}
