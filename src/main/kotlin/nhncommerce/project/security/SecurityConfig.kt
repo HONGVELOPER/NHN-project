0798984475002c9372870a8e5db87b047ca3a16d
Youngjin Hong <hyjinn1003@naver.com>
2022-08-09 17:44:12 +0900

    feat : 어드민 회원 관리 FE, BE 구현 및 layout 수정

--- src/main/kotlin/nhncommerce/project/security/SecurityConfig.kt
+++ src/main/kotlin/nhncommerce/project/security/SecurityConfig.kt
@@ -50,6 +50,7 @@ class SecurityConfig(
             .and()
             .and()
             .logout()
+            .logoutSuccessUrl("/user")
             .and()
             .sessionManagement()
             .maximumSessions(1)
