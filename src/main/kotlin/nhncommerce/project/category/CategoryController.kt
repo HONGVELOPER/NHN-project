7e0882c58fa4ffdc0a500282f88fa6536525b494
정순범 <jsb12302@naver.com>
2022-08-12 21:34:31 +0900

    feat : categorySort
    카테고리 정렬, 페이지 이동시 정렬 유지

--- src/main/kotlin/nhncommerce/project/category/CategoryController.kt
+++ src/main/kotlin/nhncommerce/project/category/CategoryController.kt
@@ -6,6 +6,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("categories")
@@ -28,6 +29,7 @@ class CategoryController(private val categoryService: CategoryService) {
         model.addAttribute("products", products)
         model.addAttribute("parentCategoryDTO", parentCategoryDTO)
         model.addAttribute("childCategoryList", childCategoryList)
+        model.addAttribute("type", pageRequestDTO.type)
         return "category/category"
     }
 }
