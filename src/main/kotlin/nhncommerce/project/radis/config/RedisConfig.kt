d0717cee14d850487845f2134697b014570b35d0
유재형 <wogud758787@naver.com>
2022-08-12 12:30:31 +0900

    feat : redis config 파일

---
+++ src/main/kotlin/nhncommerce/project/radis/config/RedisConfig.kt
@@ -0,0 +1,34 @@
+package nhncommerce.project.radis.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.CacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory() : RedisConnectionFactory {
+        return LettuceConnectionFactory("localhost", 6379)
+    }
+
+    @Bean
+    fun redisTemplate() : RedisTemplate<*, *> {
+        val redisTemplate: RedisTemplate<*, *> = RedisTemplate<Any, Any>()
+        redisTemplate.setKeySerializer(StringRedisSerializer()) //저장시 이름관련
+        redisTemplate.setValueSerializer(GenericJackson2JsonRedisSerializer())
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        return redisTemplate
+    }
+
+}
