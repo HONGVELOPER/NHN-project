f6a753d00a73d1ee75a7f23e8cf83a89a17f2738
유재형 <wogud758787@naver.com>
2022-08-13 18:07:52 +0900

    fix : 상품상세에서 빈 이미지 업로드 오류 수정

--- src/main/kotlin/nhncommerce/project/product/ProductImageRepository.kt
+++ src/main/kotlin/nhncommerce/project/product/ProductImageRepository.kt
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface ProductImageRepository : JpaRepository<ProductImage,Long>{
 
-    fun findByProduct(product: Product) : List<ProductImage>
+    fun findByProduct(product: Product) : List<ProductImage>?
 
 }
