b7b2b9ec5e2b1e74bea2de3b7251a7c437ad8ef7
정순범 <jsb12302@naver.com>
2022-08-13 22:24:02 +0900

    feat : 스케줄링, 스프링 배치 어노테이션 추가

--- src/main/kotlin/nhncommerce/project/ProjectApplication.kt
+++ src/main/kotlin/nhncommerce/project/ProjectApplication.kt
@@ -1,11 +1,15 @@
 package nhncommerce.project
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
+@EnableBatchProcessing
 class ProjectApplication
 
 fun main(args: Array<String>) {
