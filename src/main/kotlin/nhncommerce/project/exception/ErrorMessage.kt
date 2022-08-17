88bd7403fd4a59d387cdd6092cdb17db313eda80
정순범 <jsb12302@naver.com>
2022-08-17 10:52:18 +0900

    refactor : 에러 메세지 추가

--- src/main/kotlin/nhncommerce/project/exception/ErrorMessage.kt
+++ src/main/kotlin/nhncommerce/project/exception/ErrorMessage.kt
@@ -11,6 +11,9 @@ enum class ErrorMessage(
     CHANGE_TO_ORIGIN_PASSWORD("기존 비밀번호와 일치하는 비밀번호로 수정할 수 없습니다.", "/api/users/updatePasswordForm"),
     INCORRECT_NEW_PASSWORD("새로운 비밀번호가 일치하지 않습니다.", "/api/users/updatePasswordForm"),
 
+    //이미지 업로드
+    IMAGE_UPLOAD_FAILED("이미지 업로드 실패 입니다.","/admin/products"),
+    IMAGE_DELETE_FAILED("이미지 삭제 실패 입니다.","/admin/products")
 
 
 }
