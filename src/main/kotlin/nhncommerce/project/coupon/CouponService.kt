629360e0cf6f5829c629141844354c498adbf9a5
유재형 <wogud758787@naver.com>
2022-08-23 00:39:24 +0900

    fix : 상품 구매시 쿠폰 정보에 할인률, 만료일 추가

--- src/main/kotlin/nhncommerce/project/coupon/CouponService.kt
+++ src/main/kotlin/nhncommerce/project/coupon/CouponService.kt
@@ -65,7 +65,7 @@ class CouponService(
         val user = userRepository.findById(userId).get()
         val couponList = couponRepository.findByUser(user)
         return couponList.map {
-            CouponListViewDTO(it.couponId, it.couponName, it.expired, it.status)
+            CouponListViewDTO(it.couponId, it.couponName, it.expired, it.status, it.discountRate)
         }
     }
 
