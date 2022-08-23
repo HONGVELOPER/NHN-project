b86f7b31f28247b24b255d5e213c42a87ecbe7cc
정순범 <jsb12302@naver.com>
2022-08-23 17:55:48 +0900

    refactor : 2번 서버 배치

--- src/main/kotlin/nhncommerce/project/sales/scheduler/SalesScheduler.kt
+++ src/main/kotlin/nhncommerce/project/sales/scheduler/SalesScheduler.kt
@@ -9,6 +9,7 @@ import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.net.InetAddress
 import java.time.LocalDateTime
 
 @Component
@@ -19,17 +20,21 @@ class Scheduler(
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-        @Scheduled(cron = "0 0 05 * * *") //매일 새벽 5시 실행
+    @Scheduled(cron = "0 0 05 * * *") //매일 새벽 5시 실행
     fun executeJob() {
-        try {
-            jobLauncher.run(
-                salesJob.salesJob(), JobParametersBuilder()
-                    .addString("datetime", LocalDateTime.now().toString()) //job의 유일한 id로 같으면 안되기 때문
-                    .toJobParameters() // job parameter 설정
-            )
-        } catch (ex: JobExecutionException) {
-            log.info("일 매출 계산 스케줄 작업 실패")
+        val serverIp = InetAddress.getLocalHost().hostAddress
+        if(serverIp == "192.168.0.118"){
+            try {
+                jobLauncher.run(
+                    salesJob.salesJob(), JobParametersBuilder()
+                        .addString("datetime", LocalDateTime.now().toString()) //job의 유일한 id로 같으면 안되기 때문
+                        .toJobParameters() // job parameter 설정
+                )
+            } catch (ex: JobExecutionException) {
+                log.info("일 매출 계산 스케줄 작업 실패")
+            }
         }
+
     }
 
 }
